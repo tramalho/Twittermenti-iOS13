d41725407df7964c7941a4965c64357e58d3fc37
tramalho <thiagofacul@gmail.com>
2021-12-28 22:09:35 -0300

    355. Setting Up the Swifter Framework

--- Twittermenti/ViewController.swift
+++ Twittermenti/ViewController.swift
@@ -7,15 +7,37 @@
 //
 
 import UIKit
+import SwifteriOS
 
 class ViewController: UIViewController {
     
     @IBOutlet weak var backgroundView: UIView!
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var sentimentLabel: UILabel!
+    
+    // Instantiation using Twitter's OAuth Consumer Key and secret
+    private let swifter: Swifter? = {
+        
+        if let path = Bundle.main.path(forResource: "secrets", ofType: "plist") {
+            let dict = NSDictionary(contentsOfFile: path) as? [String: String]
+            
+            if let consumerKey = dict?["APIKey"], let consumerSecret = dict?["APIKeySecret"] {
+                return Swifter(consumerKey: consumerKey, consumerSecret: consumerSecret)
+            }
+        }
+        
+        return nil
+    }()
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        swifter?.searchUsers(using: "@Apple", success: { result in
+            print(result)
+        }, failure: { error in
+            print("Error: \(error.localizedDescription)")
+        })
         
     }
 
